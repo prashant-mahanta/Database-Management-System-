4f3be05ac50a28c3d22d386a26324ef123141e6b
PRASHANT KUMAR MAHANTA <prashantkumar.m16@iiits.in>
2018-03-31 21:37:10 +0530

    Update cancellation.php

--- project1/cancellation.php
+++ project1/cancellation.php
@@ -1,7 +1,7 @@
 <?php
   $servername = "localhost";
   $username = "root";
-  $password = "prashant";
+  $password = "*****"; // enter your password
   $dbName="train_test";
   // Create connection
   $conn = new mysqli($servername, $username, $password,$dbName);
