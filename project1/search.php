336e49989808c22818262e61e9e7f81af65b7ffc
PRASHANT KUMAR MAHANTA <prashantkumar.m16@iiits.in>
2018-03-31 21:38:05 +0530

    Update search.php

--- project1/search.php
+++ project1/search.php
@@ -1,7 +1,7 @@
 <?php
     $key=$_GET['from'];
     $array = array();
-    $con=mysql_connect("localhost","root","prashant");
+    $con=mysql_connect("localhost","root","*****"); // enter your password
     $db=mysql_select_db("train_test",$con);
     $query=mysql_query("select station from train_path where station LIKE '{$key}%'");
     while($row=mysql_fetch_assoc($query))
